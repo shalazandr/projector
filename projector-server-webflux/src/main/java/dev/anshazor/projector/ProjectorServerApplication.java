bda6a956162c593b81b6a8ba991b13b897456051
Andrii Shalankevych <shalankevych@gmail.com>
2022-01-17 23:42:51 +0200

    Init projector-server-webflux

---
+++ projector-server-webflux/src/main/java/dev/anshazor/projector/ProjectorServerApplication.java
@@ -0,0 +1,13 @@
+package dev.anshazor.projector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectorServerApplication.class, args);
+	}
+
+}
---
+++ projector-server-webflux/src/test/java/dev/anshazor/projector/ProjectorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anshazor.projector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
